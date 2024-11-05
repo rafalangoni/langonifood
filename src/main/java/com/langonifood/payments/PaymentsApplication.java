25d06033616b3952180d3784613d9a2f80f663a1
Rafael Langoni <langoninaves@gmail.com>
2024-11-05 06:47:16 -0330

    using Eureka to do service discovery and service Registry

--- src/main/java/com/langonifood/payments/PaymentsApplication.java
+++ src/main/java/com/langonifood/payments/PaymentsApplication.java
@@ -2,12 +2,15 @@ package com.langonifood.payments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PaymentsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaymentsApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentsApplication.class, args);
+    }
 
 }

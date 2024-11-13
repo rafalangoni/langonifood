3356c5abb2faa5c9a3eba6ad31478b4992ffc623
Rafael Langoni <langoninaves@gmail.com>
2024-11-13 06:11:05 -0330

    implementing queue wth RabbitMQ

---
+++ src/main/java/com/langonifood/payments/amqp/PaymentAMQPConfiguration.java
@@ -0,0 +1,31 @@
+package com.langonifood.payments.amqp;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PaymentAMQPConfiguration {
+
+    @Bean
+    public Queue createQueue(){
+        //return new Queue("payment.finished", false);
+
+        return QueueBuilder.nonDurable("payment.finished").build();
+    }
+
+    @Bean
+    public RabbitAdmin createRabbitAdmin(ConnectionFactory conn){
+        return new RabbitAdmin(conn);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> initializeAdmin(RabbitAdmin rabbitAdmin){
+        return event -> rabbitAdmin.initialize();
+    }
+}
